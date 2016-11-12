ef239227ef7fdcae21905517ffec4ede9e7137c2
Vytenis <vytenis.butkevicius@gmail.com>
2016-11-12 21:51:23 +0200

    Disable custom logging dir for PM2 until find solution with persmisions

--- run.js
+++ run.js
@@ -1,17 +1,18 @@
 const pm2 = require('pm2');
 
-pm2.connect(false, err => {
+pm2.connect(err => {
     if (err) {
         console.log(err);
         process.exit(2);
     }
 
+    console.log('Starting server');
     pm2.start({
         script: 'index.js',
         name: 'tellor',
         max_memory_restart: '6000M',
-        output: '/var/logs/tellor.pm2.logs',
-        error: '/var/logs/tellor.pm2.error.logs'
+        // output: '/var/log/tellor/tellor.pm2.log',
+        // error: '/var/log/tellor/tellor.pm2.error.log'
     }, err => {
         pm2.disconnect();
         if (err) throw err;
