e79f6c9c898f088331eb6a521ccc99e7691d7b9a
Vytenis <vytenis.butkevicius@gmail.com>
2016-11-12 23:11:36 +0200

    Fix typo and formatting

--- pm2.config.js
+++ pm2.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
 
-    // First application
-    {
-      name      : "tellor",
-      script    : __dirname + "index.js",
-      max_memory_restart: '6000M',
-      env_production : {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        // First application
+        {
+            name: 'tellor',
+            script: __dirname + '/index.js',
+            max_memory_restart: '6000M',
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
 };
